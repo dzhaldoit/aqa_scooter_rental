69f4d87c9af69da42ae4792c61dfdbbe25e6c3f7
Dzhalal <djaldoit@gmai.com>
2024-08-06 11:04:18 +0300

    test 0.3

--- tests/mobile_tests/conftest.py
+++ tests/mobile_tests/conftest.py
@@ -1,15 +1,63 @@
-import os
-
+# import os
+#
+# import allure
+# import pytest
+# from dotenv import load_dotenv
+# from appium.options.android import UiAutomator2Options
+# from selene import browser
+# from appium import webdriver
+# from aqa_scooter_rental.utils import attach
+#
+#
+# @pytest.fixture(scope="session", autouse=True)
+# def load_env():
+#     load_dotenv()
+#
+#
+# context = os.getenv('context', 'bstack')
+# username = os.getenv('USER_NAME')
+# access_key = os.getenv('ACCESS_KEY')
+# remote_browser_url = os.getenv('REMOTE_BROWSER_URL')
+# bstack_app_id = os.getenv('BSTACK_APP_ID')
+#
+#
+# @pytest.fixture(scope='function')
+# def android_mobile_management():
+#     options = UiAutomator2Options().load_capabilities({
+#         # Specify device and os_version for testing
+#         "platformName": "android",
+#         "platformVersion": "12.0",
+#         "deviceName": "Samsung Galaxy S22 Ultra",
+#
+#         # Set URL of the application under test
+#         "app": bstack_app_id,
+#
+#         # Set other BrowserStack capabilities
+#         'bstack:options': {
+#             "projectName": "Android tests",
+#             "buildName": "browserstack-scooter-rent",
+#             "sessionName": "BStack scooter-rent",
+#
+#             # Set your access credentials
+#             "userName": username,
+#             "accessKey": access_key
+#         }
+#     })
+#     browser.config.driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
+#
+#     browser.config.timeout = 10.0
 import allure
 import pytest
-from dotenv import load_dotenv
+import os
+
 from appium.options.android import UiAutomator2Options
-from selene import browser
 from appium import webdriver
+from selene import browser, support
+from dotenv import load_dotenv
 from aqa_scooter_rental.utils import attach
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 def load_env():
     load_dotenv()
 
@@ -22,35 +70,32 @@ bstack_app_id = os.getenv('BSTACK_APP_ID')
 
 
 @pytest.fixture(scope='function')
-def android_mobile_management():
-    options = UiAutomator2Options().load_capabilities({
-        # Specify device and os_version for testing
-        "platformName": "android",
-        "platformVersion": "12.0",
-        "deviceName": "Samsung Galaxy S22 Ultra",
+def mobile_management():
+    load_dotenv()
+    with allure.step('Configurate options'):
+        options = UiAutomator2Options().load_capabilities({
+            "platformName": "android",
+            "platformVersion": "12.0",
+            "deviceName": "Samsung Galaxy S22 Ultra",
 
-        # Set URL of the application under test
-        "app": bstack_app_id,
+            # Set URL of the application under test
+            "app": bstack_app_id,
 
-        # Set other BrowserStack capabilities
-        'bstack:options': {
-            "projectName": "Android tests",
-            "buildName": "browserstack-scooter-rent",
-            "sessionName": "BStack scooter-rent",
+            # Set other BrowserStack capabilities
+            'bstack:options': {
+                "projectName": "Android tests",
+                "buildName": "browserstack-scooter-rent",
+                "sessionName": "BStack scooter-rent",
 
-            # Set your access credentials
-            "userName": username,
-            "accessKey": access_key
-        }
-    })
+                "userName": username,
+                "accessKey": access_key,
+            }
+        })
 
-    driver = webdriver.Remote(
-        command_executor=remote_browser_url,
-        options=options
-    )
-
-    browser.config.timeout = 10.0
-    browser.config.driver = driver
+    browser.config.driver = webdriver.Remote(
+        remote_browser_url,
+        options=options)
+    browser.config.timeout = float(os.getenv("TIMEOUT"))
 
     yield browser.config.driver
 
--- tests/mobile_tests/test_mobile.py
+++ tests/mobile_tests/test_mobile.py
@@ -3,10 +3,12 @@ from aqa_scooter_rental.pages.mobile.couriers_pages import mobile_pages
 from tests.api_tests.conftest import currier_data
 
 
-
 class TestMobile:
-    def test_bstack_android(self, currier_data):
+
+    def test_api_take_order(self, currier_data):
         api_pages.order_take_successful(currier_data)
+
+    def test_bstack_android(self, mobile_management):
         mobile_pages.connecting_server()
         mobile_pages.test_login_courier()
         mobile_pages.completed_orders()
