fc894092bb4c60a44637b533d254c3e738de639f
Dzhalal <djaldoit@gmai.com>
2024-08-09 18:15:36 +0300

    test 0.9

--- aqa_scooter_rental/pages/ui/order_details_page.py
+++ aqa_scooter_rental/pages/ui/order_details_page.py
@@ -46,6 +46,7 @@ class OrderPage:
     @allure.step("Заполнение поля Метро")
     def metro(self, metro):
         browser.element(*OrderLocators.METRO).send_keys(metro)
+        browser.element(*OrderLocators.LIST_STATION).should(be.clickable).click()
         return self
 
     @allure.step("Заполнение поля Телефон")
